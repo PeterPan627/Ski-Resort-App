2c2ddf17d6ddc1528c6cc22017605c165b6d0153
LeiMing627 <jackperry933jp@gmail.com>
2019-10-31 21:48:56 -0400

    Minor changes to make code and bugs more clear

--- assets/javascript/foursquare.js
+++ assets/javascript/foursquare.js
@@ -12,26 +12,27 @@ var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/busi
   }).then(function(response) {
 
     //Using jQuery, creates a div that will eventually be sent to the html site under "resortDiv"
-    var resortsDisplay = $("<div class='row'>");
-    resortsDisplay.append("<div class='col-md-2'></div>");
-
+    var resortsDisplay = $("<div class='col-md-2'> Why isn't this an empty column </div>");
 
     console.log(response);
+
+    //builds (should build, but the bootstrap syntax must be off) the html for the div that displays the first 4 resorts' names, phone #, rating, and coords
     for(var i=0; i<4; i++){
         var name = response.businesses[i].name;
         var longitude = response.businesses[i].coordinates.latitude;
         var latitude = response.businesses[i].coordinates.longitude;
-        var imageURL = response.businesses[i].imageURL;
+        var imageURL = response.businesses[i].image_url;
         var phone = response.businesses[i].display_phone;
         var rating = response.businesses[i].rating;
         resortsDisplay.append("<div class='col-md-2'>")
         resortsDisplay.append("<strong>" + name + "</strong> <br/>");
-        resortsDisplay.append("<img src= '" + imageURL + "'> <br/>");
+      //  resortsDisplay.append("<img src= '" + imageURL + "'> <br/>");
+      //I have the image tag above commented out because they're too large and need to be resized. Should be fairly straightforward bootstrap/css to add?
         resortsDisplay.append(phone + "<br />");
         resortsDisplay.append("Rating: " + rating + "<br/>");
         resortsDisplay.append("Lat: " + latitude + "<br/>");
-        resortsDisplay.append("Long: " + longitude + "<br/>");
-        resortsDisplay.append("</div>");
+        resortsDisplay.append("Long: " + longitude + "<br />");
+        resortsDisplay.append("</div> This is adding so the /div should be adding");
     }
     resortsDisplay.append("</div>");
     
